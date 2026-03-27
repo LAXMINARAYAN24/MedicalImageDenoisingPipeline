0eae8b0d229c985bcd44b4f0d25d5f7b378db5c1
LAXMINARAYAN24 <sahulucky2411@gmail.com>
2026-03-27 14:48:48 +0530

    Phase 4: Add CNN classifier for Normal/Abnormal classification
    - 3-block ConvNet with BatchNorm + ReLU per block
    - GlobalAvgPool to remove spatial dimensions
    - FC head: 128->256->2 with 0.5 dropout
    - 320,866 trainable parameters

---
+++ src/models/classifier.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn as nn
+
+
+class MedicalImageClassifier(nn.Module):
+    """
+    CNN Classifier for binary classification:
+    Normal vs. Abnormal medical images
+    """
+
+    def __init__(self, in_channels=1, num_classes=2):
+        super(MedicalImageClassifier, self).__init__()
+
+        # Block 1
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2)
+        )
+
+        # Block 2
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2)
+        )
+
+        # Block 3
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2)
+        )
+
+        # Global average pooling
+        self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+
+        # Classification head
+        self.fc = nn.Sequential(
+            nn.Linear(128, 256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.global_avg_pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
